b9b4e77b43ac63092c23a1b25e72c6802357e5eb
Morgas <morgas01@web.de>
2016-10-19 11:42:18 +0200

    ProcessArgs

--- config.js
+++ config.js
@@ -1,7 +1,28 @@
-var config=module.exports={
-	port:8081,
-	appDir:"apps",
-	apps:null
-};
+require("Morgas");
+(function(µ,SMOD,GMOD,HMOD,SC){
+	SC=SC({
+		parse:"ProcessArgs",
+		adopt:"adopt"
+	});
 
-//TODO pars process args
+	var logger=require("./logger")("main");
+
+	var config=module.exports=SC.parse({
+		port:{
+			names:["port","/p"],
+			type:"number",
+			value:8081,
+		},
+		appDir:{
+			names:["appDir","/d"],
+			value:"apps",
+		},
+		apps:{
+			type:"json",
+			names:["apps", "/a"],
+			value:null
+		}
+	});
+	logger.info({config:config},"config");
+
+})(Morgas,Morgas.setModule,Morgas.getModule,Morgas.hasModule,Morgas.shortcut);
