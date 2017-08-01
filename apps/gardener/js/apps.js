9d2d81172c833e6f6ac28be370600369a0a0a220
Morgas <morgas01@web.de>
2017-08-01 17:25:46 +0200

    refactor tableData

--- apps/gardener/js/apps.js
+++ apps/gardener/js/apps.js
@@ -1,7 +1,17 @@
 (function(µ,SMOD,GMOD,HMOD,SC){
 
-	var TableData=GMOD("gui.TableData");
-	var selectionTable=GMOD("gui.selectionTable");
+	var Select=GMOD("gui.TableConfig.Select");
+	var tConfig=new Select([
+		{name:"name",fn:(cell,data)=>cell.innerHTML=String.raw`<a href="/${data.name}" target="_top">${data.name}</a>`},
+		{name:"ID",getter:app=>app.id},
+		"state",
+		{name:"dependencies",getter:app=>app.dependencies?app.dependencies.join("\n"):""},
+		"path"],
+		{
+			noInput:true,
+			control:true
+		}
+	);
 	var request=GMOD("request");
 
 	var tableContainer=document.querySelector("#apps");
@@ -11,12 +21,7 @@
 		request.json("rest/app/list").then(function(data)
 		{
 			if(appTable) appTable.remove();
-			appTable=selectionTable(new TableData(data,[
-				{name:"name",fn:(cell,data)=>cell.innerHTML=String.raw`<a href="/${data.name}" target="_top">${data.name}</a>`},
-				{name:"ID",getter:app=>app.id},
-				"state",
-				{name:"dependencies",getter:app=>app.dependencies?app.dependencies.join("\n"):""}
-				,"path"]));
+			appTable=tConfig.getTable(data);
 			tableContainer.appendChild(appTable);
 		},
 		function()
