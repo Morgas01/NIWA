1133364eed03c0a0baf53670b3a50af325ef1e2a
Morgas <morgas01@web.de>
2017-10-31 17:55:15 +0100

    apps

--- apps/gardener/js/AppEditDialog.js
+++ apps/gardener/js/AppEditDialog.js
@@ -3,15 +3,76 @@
 	let InputDialog=GMOD("gui.InputDialog");
 
 	SC=SC({
-		Organizer:"Organizer"
+		Register:"Register"
 	});
 
-	let sort=new Intl.Collator(navigator.languages,{sensitivity:"base"}).compare
+	let sort=new Intl.Collator(navigator.languages,{sensitivity:"base"}).compare;
+	let getCommunicationSelect=function(path,name)
+	{
+		let select=document.createElement("SELECT");
+		if(path) select.dataset.path=path
+		select.name=name;
+
+		let option;
+		option=document.createElement("OPTION");
+		option.textContent="Default";
+		select.appendChild(option);
+
+		option=document.createElement("OPTION");
+		option.value=true;
+		option.textContent="All";
+		select.appendChild(option);
+
+		option=document.createElement("OPTION");
+		option.value=false;
+		option.textContent="None";
+		select.appendChild(option);
+
+		return select;
+	};
+
+	let getAppCommunication=function(name,contexts)
+	{
+		let fieldset=document.createElement("FIELDSET");
+		let legend=document.createElement("LEGEND");
+		legend.textContent=name;
+		let removeBtn=document.createElement("BUTTON");
+		addBtn.dataset.action="removeCommContext";
+		addBtn.textContent="\uD83D\uDDD1";
+		fieldset.appendChild(legend);
+
+		let select=document.createElement("SELECT");
+		for(let context of contexts)
+		{
+			let option=document.createElement("OPTION");
+			option.value=option.textContent=context;
+			select.appendChild(option);
+		}
+		fieldset.appendChild(select);
+		let addBtn=document.createElement("BUTTON");
+		addBtn.dataset.action="addCommContext";
+		addBtn.textContent="+";
+	};
+	let getAppContext=function(name,context,index)
+	{
+		let container=document.createElement("SPAN");
+		container.textContent=context;
+		let input=document.createElement("INPUT");
+		input.type="hidden";
+		input.value=context;
+		input.path="communications."+name;
+		input.name=index;
+		container.appendChild(input);
+	}
 
 	let AppEditDialog=µ.Class(InputDialog,{
 		constructor:function(appConfig,dependencies=[],appList)
 		{
-			let org=new SC.Organizer(appList).group("name","name");
+			let appNames=new SC.Register(1,Array);
+			for(let app of applist)
+			{
+				if(app.name) appNames[app.name].push(app.context);
+			}
 			let html=`
 <table>
 	<tr>
@@ -47,33 +108,16 @@
 			Communication
 		</td>
 		<td>
-			<select name="communication">
-				<option value="null" ${appConfig.communication==null?"checked":""}>default</option>
-				<option value="true" ${appConfig.communication==true?"checked":""}>all</option>
-				<option value="false" ${appConfig.communication==false?"checked":""}>none</option>
-				<option value="specific" ${typeof appConfig.communication=="object"?"checked":""}>specific</option>
+			<select class="CommunicationSelect">
+				<option></option>
+				${Object.keys(appNames).sort(sort).map(appName=>
+				`<option value="${appName}" class="${dependencies.includes(appName)?'dependency':''}">${appName}</option>
+				).join("\n")}
 			</select>
+			<button data-action="addCommApp">+</button>
+			<div class="communications"></div>
 		</td>
 	</tr>
-	${dependencies.map(function()
-	{
-		return `
-	<tr>
-		<td>
-			Communication
-		</td>
-		<td>
-			<select name="communication">
-				<option value="null" ${appConfig.communication==null?"checked":""}>default</option>
-				<option value="true" ${appConfig.communication==true?"checked":""}>all</option>
-				<option value="false" ${appConfig.communication==false?"checked":""}>none</option>
-				<option value="specific" ${typeof appConfig.communication=="object"?"checked":""}>specific</option>
-			</select>
-		</td>
-	</tr>
-`	;
-	}).join("\n")
-	}
 </table>
 `			;
 
