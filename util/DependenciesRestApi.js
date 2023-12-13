6d041b694df2483e7c4e3c96120d6251d9ee81c1
Morgas <morgas01@web.de>
2023-12-13 21:08:43 +0100

    DependenciesRestApi getFull

--- util/DependenciesRestApi.js
+++ util/DependenciesRestApi.js
@@ -2,20 +2,23 @@
 
 	SC=SC({
 		DependencyResolver:"DepRes",
+		ServiceResult:"ServiceResult",
 		File:"File"
 	});
+	let urlsToLoaderScript=function({urls,path,fullPath})
+	{
+		let restPath=fullPath.slice(0,fullPath.length-path.length).join("/");
+		let loaderScript=urls.map(f=>`document.write('<script type="text/javascript" defer="defer" src="rest/${restPath}/get/${f}"></script>')`).join("\n");
+		return new SC.ServiceResult({data:loaderScript,headers:{"Content-Type":"application/javascript; charset=utf-8"}})
+	}
 
 	module.exports = function DependenciesRestApi(dependencyManager)
 	{
 		let api=async function({path,fullPath})
 		{
-			if(path.length==1 && path[0]==="allDependencies")
+			if(path[0] in api)
 			{
-				return api.allDependencies();
-			}
-			else if (path[0]==="get")
-			{
-				return api.get(path.slice(1))
+				return api[path[0]]({path:path.slice(1),fullPath});
 			}
 			let url=path.join("/");
 			let allDependencies=await dependencyManager.getAllDependencies();
@@ -24,16 +27,14 @@
 				//TODO use DependencyManager.resolve() ?
 				let resolver=new SC.DependencyResolver(allDependencies.urlDependencies);
 				let urls=resolver.resolve(url);
-				//TODO return import script;
-				let restPath=fullPath.slice(0,fullPath.length-path.length).join("/");
-				return urls.map(f=>`document.write('<script type="text/javascript" defer="defer" src="rest/${restPath}/get/${f}"></script>')`).join("\n");
+				return urlsToLoaderScript({urls,path,fullPath});
 			}
 			else
 			{
 				return new SC.ServiceResult({data:`alert("file '${url}' not found")`,headers:{"Content-Type":"application/javascript; charset=utf-8"}});
 			}
 		};
-		api.get=async function(path)
+		api.get=async function({path}={})
 		{
 			let url=path.join("/");
 			let allDependencies=await dependencyManager.getAllDependencies();
@@ -49,6 +50,12 @@
 		api.allDependencies=function ()
 		{
 			return dependencyManager.getAllDependencies();
+		};
+		api.getFull=async function({path,fullPath})
+		{
+			let correctedFullPath=fullPath.slice();
+			correctedFullPath.splice(fullPath.length-path.length-1,1);
+			return urlsToLoaderScript({urls:(await dependencyManager.resolveAll()).urls,path,fullPath:correctedFullPath});
 		}
 		return api;
 	};
